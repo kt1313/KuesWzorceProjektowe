2dce489504d036a275d2fcbd989dc430e10e56a4
kt1313 <tomaklim@wp.pl>
2021-09-07 18:33:19 +0200

    Lekcja 8 - Wzorzec Budowniczego, jak go inicjujemy

---
+++ src/pl/strefakursow/House.java
@@ -0,0 +1,15 @@
+package pl.strefakursow;
+
+public class House {
+    String adress;
+    Integer floorsNumber;
+    Integer doorsNumber;
+    Integer windowsNumber;
+
+    public House(String adress, Integer floorsNumber, Integer doorsNumber, Integer windowsNumber) {
+        this.adress = adress;
+        this.floorsNumber = floorsNumber;
+        this.doorsNumber = doorsNumber;
+        this.windowsNumber = windowsNumber;
+    }
+}
