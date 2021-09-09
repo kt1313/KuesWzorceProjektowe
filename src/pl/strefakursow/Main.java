2c5ce4bf07d55b0526cd92486863e9b934951215
kt1313 <tomaklim@wp.pl>
2021-09-09 18:49:30 +0200

    Lekcja 16 - Wzorce Czynnosciowe - Wzorzec Obserwator , tworzenie

--- src/pl/strefakursow/Main.java
+++ src/pl/strefakursow/Main.java
@@ -2,10 +2,57 @@ package pl.strefakursow;
 
 import pl.strefakursow.models.FamilyHouse;
 
+import java.util.Observable;
+import java.util.Observer;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        creationPatterns();
+
+        // obsluga interfejsu uzytkownika
+        //obsluga zmian repozytorium
+        //obslugiwac zmiany w czujnikach
+
+        //NA PEWNO NIE ODPYTUJEMY obserwowanego
+        //obiekt obserwowany informuje zaintersowane obiekty o zmianie
+
+        Observable observableValue=new Observable(){
+            @Override
+            public void notifyObservers(Object arg) {
+                super.setChanged();
+                super.notifyObservers(arg);
+            }
+        };
+
+        observableValue.addObserver(new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                System.out.println("1 "+arg.toString());
+            }
+
+
+        });
+
+        observableValue.addObserver(new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                System.out.println("2 " + arg.toString());
+            }
+            });
+
+        observableValue.notifyObservers(54);
+
+
+
+
+
+
+
+    }
+
+    private static void creationPatterns() {
         House house = new House.HouseBuilder()
                 .setAdress("Za Zakretem 3")
                 .setDoorsNumber(3)
@@ -21,10 +68,10 @@ public class Main {
 
         FamilyHouse familyHouse = FamilyHouse.from(house);
 
-        Logger.getInstance().logToConsole(); {
+        Logger.getInstance().logToConsole();
+        {
             //
         }
-
     }
 
 }
