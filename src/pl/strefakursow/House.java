dee81875f29cad573799e720a2e03f976201da2b
kt1313 <tomaklim@wp.pl>
2021-09-09 02:40:29 +0200

    Lekcja 6 - WzorzecBudowniczy-Jak go uzywac, tworzenie buildera

--- src/pl/strefakursow/House.java
+++ src/pl/strefakursow/House.java
@@ -6,10 +6,42 @@ public class House {
     Integer doorsNumber;
     Integer windowsNumber;
 
-    public House(String adress, Integer floorsNumber, Integer doorsNumber, Integer windowsNumber) {
+    private House(String adress, Integer floorsNumber, Integer doorsNumber, Integer windowsNumber) {
         this.adress = adress;
         this.floorsNumber = floorsNumber;
         this.doorsNumber = doorsNumber;
         this.windowsNumber = windowsNumber;
+    }
+
+    public static class HouseBuilder {
+        private String adress ;
+        private Integer floorsNumber=1;
+        private Integer doorsNumber=1;
+        private Integer windowsNumber=4;
+
+        public HouseBuilder setAdress(String adress) {
+            this.adress = adress;
+            return this;
+        }
+
+        public HouseBuilder setFloorsNumber(Integer floorsNumber) {
+            this.floorsNumber = floorsNumber;
+            return this;
+        }
+
+        public HouseBuilder setDoorsNumber(Integer doorsNumber) {
+            this.doorsNumber = doorsNumber;
+            return this;
+        }
+
+        public HouseBuilder setWindowsNumber(Integer windowsNumber) {
+            this.windowsNumber = windowsNumber;
+            return this;
+        }
+
+        public House build() {
+            return new House(adress, floorsNumber, doorsNumber, windowsNumber);
+        }
+
     }
 }
--- src/pl/strefakursow/Main.java
+++ src/pl/strefakursow/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        House house=new House.HouseBuilder()
+                .setAdress("Za Zakretem 3")
+                .setDoorsNumber(3)
+                .setFloorsNumber(4)
+                .setWindowsNumber(10)
+                .build();}
     }
-}
+
